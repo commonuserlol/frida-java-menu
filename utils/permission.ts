c672e94bf60abc028167c0e795d84cae11c625df
commonuserlol <commonuserlol@users.noreply.github.com>
2023-09-27 21:36:54 +0300

    Check permission by `this.name`

--- utils/permission.ts
+++ utils/permission.ts
@@ -1,14 +1,14 @@
 namespace Menu {
     export type Permission = {
         name: string,
-        ask: () => void
+        ask: () => void,
         check: () => boolean
     }
     export const overlay: Permission = {
         name: "android.settings.action.MANAGE_OVERLAY_PERMISSION",
         ask() {
             toast(Menu.theme.noOverlayPermissionText, 1);
-            const intent = Api.Intent.$new(Api.JavaString.$new("android.settings.action.MANAGE_OVERLAY_PERMISSION"));
+            const intent = Api.Intent.$new(Api.JavaString.$new(this.name));
             intent.setFlags(Api.Intent.FLAG_ACTIVITY_NEW_TASK.value);
             intent.setData(Api.Uri.parse("package:" + app.packageName));
             context.startActivity(intent);
