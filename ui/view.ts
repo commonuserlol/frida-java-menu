ce41e135963916ce2a7622113a942f3951dcb5db
commonuserlol <commonuserlol@users.noreply.github.com>
2023-11-23 22:54:54 +0200

    Add impl for View

---
+++ ui/view.ts
@@ -0,0 +1,116 @@
+namespace Menu {
+    export class View {
+        public instance: Java.Wrapper;
+
+        public constructor (handleOrInstance?: NativePointerValue | Java.Wrapper) {
+            handleOrInstance ? this.instance = Java.cast(handleOrInstance, Api.View) : null;
+        }
+        /** Gets alpha */
+        get alpha() {
+            return this.instance.getAlpha();
+        }
+        /** Gets background */
+        get background(): Java.Wrapper {
+            return this.instance.getBackground();
+        }
+        /** Gets layout params */
+        get layoutParams(): Java.Wrapper {
+            return this.instance.getLayoutParams();
+        }
+        /** Gets orientation */
+        get orientation(): number {
+            return this.instance.getOrientation();
+        }
+        /** Gets padding */
+        get padding(): number[] {
+            return [this.instance.getPaddingLeft(), this.instance.getPaddingTop(), this.instance.getPaddingRight(), this.instance.getPaddingBottom()];
+        }
+        /** Gets text */
+        get text(): string {
+            return Java.cast(this.instance, Api.TextView).getText().toString();
+        }
+        /** Gets text color */
+        get textColor(): Java.Wrapper {
+            return this.instance.getTextColors();
+        }
+        /** Gets visibility */
+        get visibility(): number {
+            return this.instance.getVisibility();
+        }
+        /** Sets alpha */
+        set alpha(alpha: number) {
+            this.instance.setAlpha(alpha);
+        }
+        /** Sets background color */
+        set backgroundColor(color: Java.Wrapper | number) {
+            this.instance.setBackgroundColor(color);
+        }
+        /** Sets layout params */
+        set layoutParams(params: Java.Wrapper) {
+            this.instance.setLayoutParams(params);
+        }
+        /** Sets orientation */
+        set orientation(orientation: number) {
+            this.instance.setOrientation(orientation);
+        }
+        /** Sets padding */
+        set padding(position: [left: number, top: number, right: number, bottom: number]) {
+            this.instance.setPadding(...position);
+        }
+        /** Sets text */
+        set text(text: string) {
+            this.instance.setText(wrap(text));
+        }
+        /** Sets text color */
+        set textColor(color: Java.Wrapper | number) {
+            this.instance.setTextColor(color);
+        }
+        /** Sets visibility */
+        set visibility(visibility: number) {
+            this.instance.setVisibility(visibility);
+        }
+        /** Sets onClickListener callback */
+        set onClickListener(callback: () => void) {
+            this.instance.setOnClickListener(Java.registerClass({
+                name: randomString(35),
+                implements: [Api.OnClickListener],
+                methods: {
+                    onClick: callback
+                }
+            }).$new());
+        }
+        /** Sets onLongClickListener callback */
+        set onLongClickListener(callback: () => void) {
+            this.instance.setOnLongClickListener(Java.registerClass({
+                name: randomString(35),
+                implements: [Api.OnLongClickListener],
+                methods: {
+                    onLongClick: (view: Java.Wrapper) => {
+                        callback();
+                        return true;
+                    }
+                }
+            }).$new());
+        }
+        /** Sets onTouchListener callback */
+        set onTouchListener(callback: (view: Java.Wrapper, event: Java.Wrapper) => void) {
+            this.instance.setOnTouchListener(Java.registerClass({
+                name: randomString(35),
+                implements: [Api.OnTouchListener],
+                methods: {
+                    onTouch: callback
+                }
+            }).$new());
+        }
+        /** Disposes this object */
+        destroy() {
+            sleep().then(() => this.instance.$dispose());
+        }
+    }
+    
+    /** Wraps text from HTML */
+    export function wrap(text: string): Java.Wrapper
+    {
+        return Api.HTML.fromHtml(Api.JavaString.$new(String(text)));
+    }
+}
