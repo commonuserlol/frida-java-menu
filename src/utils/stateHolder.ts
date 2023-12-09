e07e8bf158fd0adf27dea7191faa1d42823366a1
commonuserlol <commonuserlol@users.noreply.github.com>
2023-12-09 20:43:41 +0200

    Call holder when state is true

--- src/utils/stateHolder.ts
+++ src/utils/stateHolder.ts
@@ -1,7 +1,7 @@
 namespace Menu {
     export function stateHolder(state: boolean, onStateChange: (state: boolean) => void) {
         let firstSkipped = false;
-        return function() {
+        const manager = () => {
             if (!firstSkipped && state) {
                 onStateChange(true);
                 firstSkipped = true;
@@ -10,6 +10,8 @@ namespace Menu {
             if (!firstSkipped) firstSkipped = true;
             state = !state;
             onStateChange(state);
-        }
+        };
+        if (state) manager();
+        return manager;
     }
 }
