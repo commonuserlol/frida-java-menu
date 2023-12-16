8dc93bdf7ff75e2ed463a039ceb7d07fa9dd188e
commonuserlol <commonuserlol@users.noreply.github.com>
2023-12-16 22:20:57 +0200

    Remove "_" prefix for widget-specific fn
    I dont like this

--- src/layout/icon.ts
+++ src/layout/icon.ts
@@ -7,14 +7,14 @@ namespace Menu {
         }
 
         /** @internal */
-        set _imageForImageView(image: string) {
+        set imageForImageView(image: string) {
             this.instance.setScaleType(Api.ScaleType.FIT_XY.value);
             this.instance.setImageBitmap(bitmap(image));
             config.icon.alpha = Math.round(config.icon.alpha * 255);
         }
 
         /** @internal */
-        set _imageForWebView(image: string) {
+        set imageForWebView(image: string) {
             this.instance.loadData(`<html><head></head><body style=\"margin: 0; padding: 0\"><img src=\"${image}\" width=\"${config.icon.size}\" height=\"${config.icon.size}\" ></body></html>`, "text/html", "utf-8");
             this.instance.backgroundColor = Api.TRANSPARENT;
             this.instance.getSettings().setAppCacheEnabled(true);
@@ -24,7 +24,7 @@ namespace Menu {
         set image(image: string) {
             const isNormalType = this.instance.$className == Api.ImageView.$className;
             const applyDimension = Math.floor(dp(config.icon.size));
-            isNormalType ? this._imageForImageView = image : this._imageForWebView = image;
+            isNormalType ? this.imageForImageView = image : this.imageForWebView = image;
             this.alpha = config.icon.alpha;
             this.layoutParams = Layout.LinearLayoutParams(Api.WRAP_CONTENT, Api.WRAP_CONTENT);
             this.instance.getLayoutParams().height.value = applyDimension;
